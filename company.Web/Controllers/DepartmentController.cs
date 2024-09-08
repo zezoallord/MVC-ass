a65641f4d979f8d6e463684682deeb1949be0a7a
zezoallord <zeyadahmed20042020@gmail.com>
2024-09-08 17:31:46 +0300

    company.web

---
+++ company.Web/Controllers/DepartmentController.cs
@@ -0,0 +1,112 @@
+﻿using Company.Data.Models;
+using Company.Repository.Interfaces;
+using Company.Service.Interfaces;
+<<<<<<< HEAD
+using Company.Service.Interfaces.Department.Dto;
+=======
+>>>>>>> 4e7271227ef2f56002153a674b19f1451a9818c9
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace company.Web.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentService _departmentservice;
+
+        public DepartmentController(IDepartmentService departmentservice)
+        {
+
+            _departmentservice = departmentservice;
+        }
+
+<<<<<<< HEAD
+        //public IDepartmentService Departmentservice { get; }
+=======
+        public IDepartmentService Departmentservice { get; }
+>>>>>>> 4e7271227ef2f56002153a674b19f1451a9818c9
+
+        public IActionResult Index()
+        {
+            var departments = _departmentservice.GetAll();
+            return View(departments);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+<<<<<<< HEAD
+        public IActionResult Create(DepartmentDto department)
+=======
+        public IActionResult Create(Department department)
+>>>>>>> 4e7271227ef2f56002153a674b19f1451a9818c9
+        {
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+
+                    _departmentservice.Add(department);
+
+                    return RedirectToAction(nameof(Index));
+                }
+
+                ModelState.AddModelError("DepartmentError", "Validation Error");
+                return View(department);
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError("DepartmentError", ex.Message);
+                return View(department);
+
+            }
+        }
+        public IActionResult Details(int? id, string viewName = "Details")
+        {
+            var department = _departmentservice.GetbyId(id);
+
+            if (department == null)
+            {
+                return RedirectToAction("notfound", null, "Home");
+            }
+
+            return View(viewName, department);
+        }
+        [HttpGet]
+        public IActionResult Update(int? id)
+        {
+
+            return Details(id, "Update");
+        }
+        [HttpPost]
+<<<<<<< HEAD
+        public IActionResult Update(int? id, DepartmentDto department)
+        {
+            if (department.Id != id.Value)
+=======
+        public IActionResult Update(int? id, Department department)
+        {
+            if (department.id != id.Value)
+>>>>>>> 4e7271227ef2f56002153a674b19f1451a9818c9
+                return RedirectToAction("notfound", null, "Home");
+            _departmentservice.Update(department);
+
+            return RedirectToAction(nameof(Index));
+        }
+        
+        public IActionResult Delete(int id)
+        {
+            var department = _departmentservice.GetbyId(id);
+            if (department == null)
+                return RedirectToAction("notfound", null, "Home");
+            _departmentservice.Delete(department);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }   
+}
---
+++ company.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,131 @@
+﻿
+using Company.Data.Models;
+using Company.Service.Interfaces;
+<<<<<<< HEAD
+using Company.Service.Interfaces.Department.Dto;
+using Company.Service.Interfaces.Employee.Dto;
+using Company.Service.Services;
+=======
+>>>>>>> 4e7271227ef2f56002153a674b19f1451a9818c9
+using Microsoft.AspNetCore.Mvc;
+namespace company.Web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+<<<<<<< HEAD
+        private readonly IEmployeeService _employeeService;
+        private readonly IDepartmentService _departmentService;
+
+        public EmployeeController(IEmployeeService employeeService,  IDepartmentService departmentService)
+        {
+            _employeeService = employeeService;
+            _departmentService = departmentService;
+        }
+
+        public ActionResult Index(string searchInp)
+        {
+            // ViewBag.Message = "Hello From Employee Index (ViewBag)";
+            //
+            // ViewData["TextMessage"] = "Hello From Employee Index (ViewData)";
+
+
+            IEnumerable<EmployeeDto> employees = new List<EmployeeDto>();
+            if (string.IsNullOrEmpty(searchInp))
+                employees = _employeeService.GetAll();
+            else
+                employees = _employeeService.GetEmployeeByName(searchInp);
+            return View(employees);
+        }
+        public IActionResult Delete(int? id)
+        {
+            var employee = _employeeService.GetById(id);
+            if (employee is null)
+                return RedirectToAction("NotFoundPage", null, "Home");
+
+            _employeeService.Delete(employee);
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var departments = _departmentService.GetAll();
+            ViewBag.Departments = departments;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(EmployeeDto employee)
+=======
+        private readonly IEmployeeService _employeeservice;
+
+        public EmployeeController(IEmployeeService employeeservice)
+        {
+
+            _employeeservice = employeeservice;
+        }
+
+        public IEmployeeService employeeservice { get; }
+
+        public IActionResult Index()
+        {
+            var employees = _employeeservice.GetAll();
+            return View(employees);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+>>>>>>> 4e7271227ef2f56002153a674b19f1451a9818c9
+        {
+            try
+            {
+                if (ModelState.IsValid)
+<<<<<<< HEAD
+                {
+                    _employeeService.Add(employee);
+                    return RedirectToAction(nameof(Index));
+                }
+                var departments = _departmentService.GetAll();
+                ViewBag.Departments = departments;
+                return View(employee);
+            }
+            catch (Exception ex)
+            {
+                var departments = _departmentService.GetAll();
+                ViewBag.Departments = departments;
+                return View(employee);
+            }
+        }
+
+    }
+}
+=======
+            {
+                _employeeservice.Add(employee);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+
+            }
+            catch (Exception ex)
+            {
+
+
+                return View(employee);
+            }
+            
+
+        }
+        public IActionResult details(int? id)
+        {
+            var employee = _employeeservice.GetbyId(id);
+            return View(employee);
+        }
+
+    }
+
+
+}
+>>>>>>> 4e7271227ef2f56002153a674b19f1451a9818c9
